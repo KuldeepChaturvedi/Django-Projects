82e617ff472ec747e4a3f65a3371d6229cd0ee53
Kuldeep Chaturvedi <kuldeepchaturvedi475@gmail.com>
2019-05-31 00:32:49 +0530

    DAY 11

--- Django-Login/django_login/django_login/wsgi.py
+++ Django-Login/django_login/django_login/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Twitter_Sentiment_Analysis project.
+WSGI config for django_login project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Twitter_Sentiment_Analysis.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_login.settings')
 
 application = get_wsgi_application()
--- Twitter_Sentiment_Analysis/TSA/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TsaConfig(AppConfig):
-    name = 'TSA'
